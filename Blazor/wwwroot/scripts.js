fd0bc0f6517d5a9f58154fb26d4e065669ad8742
ActiveDbSoft CI Service <no-reply@activedbsoft.com>
2024-12-27 12:01:22 +0500

    Version 3.12.2.490

--- Blazor/wwwroot/scripts.js
+++ Blazor/wwwroot/scripts.js
@@ -1,13 +1,40 @@
-﻿const name = 'Blazor';
+﻿
+let sessionId = localStorage.getItem('qbSessionId');
+
+if (!sessionId) {
+    sessionId = Math.random().toString(36).substring(2, 9);
+
+    localStorage.setItem('qbSessionId', sessionId);
+}
+
+class Helpers {
+    static dotNetHelper;
+
+    static setDotNetHelper(value) {
+        Helpers.dotNetHelper = value;
+    }
+
+    static async sqlUpdated() {
+        await Helpers.dotNetHelper.invokeMethodAsync('OnSqlUpdated');
+    }
+}
+
+window.Helpers = Helpers;
 
 window.initializeQueryBuilder = () => {
-    AQB.Web.UI.QueryBuilder(name, $('#qb'));
-    AQB.Web.UI.ObjectTreeView(name, $('#treeview'));
-    AQB.Web.UI.SubQueryNavigationBar(name, $('#navbar'));
-    AQB.Web.UI.Canvas(name, $('#canvas'));
-    AQB.Web.UI.StatusBar(name, $('#statusbar'));
-    AQB.Web.UI.Grid(name, $('#grid'), { orColumnCount: 0 });
-    AQB.Web.UI.SqlEditor(name, $('#sql'));
+    AQB.Web.UI.QueryBuilder(sessionId, $('#qb'));
+    AQB.Web.UI.ObjectTreeView(sessionId, $('#treeview'));
+    AQB.Web.UI.SubQueryNavigationBar(sessionId, $('#navbar'));
+    AQB.Web.UI.Canvas(sessionId, $('#canvas'));
+    AQB.Web.UI.StatusBar(sessionId, $('#statusbar'));
+    AQB.Web.UI.Grid(sessionId, $('#grid'), { orColumnCount: 0 });
+    AQB.Web.UI.SqlEditor(sessionId, $('#sql'));
+
+    AQB.Web.onQueryBuilderReady(function (qb) {
+        qb.on(qb.Events.SqlChanged, function () {
+            window.Helpers.sqlUpdated();
+        });
+    });
 
     const connection = new signalR.HubConnectionBuilder()
         .withUrl("/query-builder/handler")
